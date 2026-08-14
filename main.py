1db414fc7c535ea7cb044610c2d73337458d09f6
Ankit Ojha <ankitojha2004@gmail.com>
2026-08-14 17:39:26 +0530

    add Query paramter and sort the data as per client request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path , HTTPException
+from fastapi import FastAPI, Path , HTTPException, Query
 import json
 
 app = FastAPI()
@@ -31,3 +31,25 @@ def view_patient(patient_id: str = Path(...,description = "ID of the patient in 
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code= 404 , detail = "Patient not found")
+
+# NOTE : three dots(...,description =  ........) makes the parameter mandaotry else optional
+
+@app.get("/sort")
+def sort_patients(sort_by:str = Query(...,description = "sort on the basis of height,weight or bmi"), 
+                order : str = Query("asc",description = "sort in asc or desc order")):
+
+    valid_feilds = ["height","weight","bmi"]
+
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code=400,detail ="invalid feild select from {valid_feilds}" )
+
+    if order not in ["asc","desc"]:
+        raise HTTPException(status_code=400,detail = "Invalid order select between asc and desc ")
+
+    data = data_load()
+
+    sort_order = True if order == "desc" else False
+
+    sorted_data = sorted(data.values(),key = lambda x: x.get(sort_by,0),reverse = sort_order)
+
+    return sorted_data
